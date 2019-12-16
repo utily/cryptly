699fbf2523490697ab331ba188ebb4d43969d15b
Simon Mika <simonmika@users.noreply.github.com>
2019-12-16 12:28:32 +0100

    Added functionality forusing splitted keys. (#2)

--- Algorithm.spec.ts
+++ Algorithm.spec.ts
@@ -2,12 +2,17 @@ import * as cryptly from "./index"
 
 describe("Algorithm", () => {
 	it("generate AES CBC 256", async () => {
-		const algorithm = cryptly.Algorithm.AesCbc(256)
+		const algorithm = cryptly.Algorithm.aesCbc(256)
 		const key = await algorithm.export()
 		expect(key).toHaveLength(43)
 	})
+	it("generate splitted AES CBC 256", async () => {
+		const algorithm = cryptly.Algorithm.aesCbc(256)
+		const keys = await algorithm.export(2)
+		expect(keys).toHaveLength(2)
+	})
 	it("encrypt & decrypt AES CBC", async () => {
-		const algorithm = cryptly.Algorithm.AesCbc(256)
+		const algorithm = cryptly.Algorithm.aesCbc(256)
 		const encrypted = await algorithm.encrypt("The meaning of life, the universe and everything.")
 		expect(encrypted.salt).toHaveLength(22)
 		expect(encrypted.value).toHaveLength(86)
@@ -15,12 +20,23 @@ describe("Algorithm", () => {
 		expect(decrypted).toEqual("The meaning of life, the universe and everything.")
 	})
 	it("encrypt & decrypt AES CBC with fixed salt", async () => {
-		const algorithm = cryptly.Algorithm.AesCbc("VaVdZS5E6s9oEdLonNVTgSKv5zAW0LNvjBSdrmnEBS8")
+		const algorithm = cryptly.Algorithm.aesCbc("VaVdZS5E6s9oEdLonNVTgSKv5zAW0LNvjBSdrmnEBS8")
 		const encrypted = await algorithm.encrypt("The meaning of life, the universe and everything.", "K1LVSh3fzl_UTQl02fIMzg")
 		expect(encrypted.salt).toEqual("K1LVSh3fzl_UTQl02fIMzg")
 		expect(encrypted.value).toEqual("VSzGsuBi-Y2L719u3ALxpvnxeAjfJxwcXsSfJSRtwWYA8-qUXBeyQo6A0ZX7QkDgnTEqM70Cdb1-eVa7izHyFQ")
 		expect(await algorithm.encrypt("The meaning of life, the universe and everything.")).not.toEqual(encrypted.value)
 		const decrypted = await algorithm.decrypt(encrypted)
 		expect(decrypted).toEqual("The meaning of life, the universe and everything.")
+	})
+	it("split keys", async () => {
+		const algorithm = cryptly.Algorithm.aesCbc(256)
+		const master = await algorithm.export()
+		expect(master).toHaveLength(43)
+		const keys = await algorithm.export(3)
+		expect(keys).toHaveLength(3)
+		keys.map(key => expect(key).toHaveLength(43))
+		const imported = await cryptly.Algorithm.aesCbc(keys).export()
+		expect(imported).toHaveLength(43)
+		expect(imported).toEqual(master)
 	})
 })
--- Algorithm.ts
+++ Algorithm.ts
@@ -13,10 +13,30 @@ export class Algorithm {
 	async decrypt(encrypted: Encrypted): Promise<string> {
 		return new TextDecoder().decode(new Uint8Array(await crypto.subtle.decrypt({ name: (await this.key).algorithm.name, iv: Base64.decode(encrypted.salt, "url") }, await this.key, Base64.decode(encrypted.value, "url"))))
 	}
-	async export(): Promise<string> {
-		return Base64.encode(new Uint8Array(await crypto.subtle.exportKey("raw", await this.key)), "url")
+	async export(): Promise<string>
+	async export(parts: number): Promise<string[]>
+	async export(parts: number = 1): Promise<string | string[]> {
+		const key = new Uint8Array(await crypto.subtle.exportKey("raw", await this.key))
+		let result: Uint8Array[] = Algorithm.generateRandomKeys(key.length, parts - 1)
+		result = [Algorithm.reduceKeys([key, ...result]), ...result]
+		return result.length == 1 ? Base64.encode(result[0], "url") : result.map(r => Base64.encode(r, "url"))
 	}
-	static AesCbc(key: string | 256): Algorithm {
-		return new Algorithm(typeof key == "number" ? crypto.subtle.generateKey({ name: "AES-CBC", length: key }, true, ["encrypt", "decrypt"]) : crypto.subtle.importKey("raw", Base64.decode(key, "url"), "AES-CBC", true, ["encrypt", "decrypt"]))
+	static aesCbc(key: 256 | string | string[]): Algorithm {
+		return new Algorithm(
+			typeof key == "number" ? crypto.subtle.generateKey({ name: "AES-CBC", length: key }, true, ["encrypt", "decrypt"]) :
+			crypto.subtle.importKey("raw",
+				Array.isArray(key) ? Algorithm.reduceKeys(key.map(k => Base64.decode(k, "url"))) :
+				Base64.decode(key, "url"),
+			"AES-CBC", true, ["encrypt", "decrypt"]))
+	}
+	private static generateRandomKeys(length: number, parts: number): Uint8Array[] {
+		return parts > 0 ? [crypto.getRandomValues(new Uint8Array(length)), ...this.generateRandomKeys(length, parts - 1)] : []
+	}
+	private static reduceKeys(keys: Uint8Array[]): Uint8Array {
+		const result = new Uint8Array(keys[0].length)
+		for (let index = 0; index < keys[0].length; index++)
+			// tslint:disable-next-line:no-bitwise
+			result[index] = keys.reduce((p, c) => p ^ c[index], 0)
+		return result
 	}
 }
