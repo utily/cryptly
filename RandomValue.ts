86ad1de9559cad9f85a7e0328799936708c0e2d9
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2020-09-14 15:01:51 +0200

    Implemented ECDSA and RSA-PSS encryption. Added Random Value generator. (#11)

---
+++ RandomValue.ts
@@ -0,0 +1,20 @@
+import { crypto } from "./crypto"
+
+export namespace RandomValue {
+	export function generate<
+		T extends
+			| Int8Array
+			| Int16Array
+			| Int32Array
+			| Uint8Array
+			| Uint16Array
+			| Uint32Array
+			| Uint8ClampedArray
+			| Float32Array
+			| Float64Array
+			| DataView
+			| null
+	>(array: T): T {
+		return crypto.getRandomValues(array)
+	}
+}
--- index.ts
+++ index.ts
@@ -3,10 +3,23 @@ import * as Base64 from "./Base64"
 import * as Base16 from "./Base16"
 import { Digest } from "./Digest"
 import { Encrypted } from "./Encrypted"
+import { Identifier } from "./Identifier"
+import { Password } from "./Password"
+import { RandomValue } from "./RandomValue"
+import { Signer } from "./Signer"
 import { TextDecoder } from "./TextDecoder"
 import { TextEncoder } from "./TextEncoder"
-import { Identifier } from "./Identifier"
-import { Signer } from "./Signer"
-import { Password } from "./Password"
 
-export { Algorithm, Base16, Base64, Digest, Encrypted, Identifier, Password, Signer, TextDecoder, TextEncoder }
+export {
+	Algorithm,
+	Base16,
+	Base64,
+	Digest,
+	Encrypted,
+	Identifier,
+	Password,
+	RandomValue,
+	Signer,
+	TextDecoder,
+	TextEncoder,
+}
