99b5bd83fb96699d16aeefac899320f3375fc574
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2020-12-20 16:18:03 +0100

    Eslint update order imports (#14)
    * modified .eslintrc and reordered imports
    
    * eslint changes

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Algorithm } from "./Algorithm"
-import * as Base64 from "./Base64"
 import * as Base16 from "./Base16"
+import * as Base64 from "./Base64"
 import { Digest } from "./Digest"
 import { Encrypted } from "./Encrypted"
 import { Identifier } from "./Identifier"
