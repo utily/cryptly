2470d3f369f2a789d4cfe137b8902e0ef7ebc880
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2021-01-19 16:49:23 +0100

    add parse and toString function for Encrypted (#19)
    * add parse and toString function for Encrypted
    
    * slice csc key name
    
    * rename
    
    * Fix name

--- Encrypted.ts
+++ Encrypted.ts
@@ -13,4 +13,13 @@ export namespace Encrypted {
 			typeof value.salt == "string"
 		)
 	}
+	export function stringify(encrypted: Encrypted): string {
+		encrypted.key = encrypted.key && encrypted.key.length != 4 ? encrypted.key.slice(-2) : encrypted.key
+		return [encrypted.key, encrypted.salt, encrypted.value].join(".")
+	}
+	export function parse(encryptedString: string): Encrypted | undefined {
+		const splitted = encryptedString.split(".")
+		const encrypted = { key: splitted[0], salt: splitted[1], value: splitted[2] }
+		return Encrypted.is(encrypted) ? encrypted : undefined
+	}
 }
