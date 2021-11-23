302cb4cd58967817e136ec95b4794ddd214d9672
Simon Mika <simon@mika.se>
2021-11-23 12:05:00 +0100

    Added handling of multiple Algorithms.

---
+++ Algorithms.ts
@@ -0,0 +1,17 @@
+import { Algorithm } from "./Algorithm"
+
+export type Algorithms = Record<string, Algorithm> & { current: Algorithm }
+
+export namespace Algorithms {
+	export function create(create: (keys: string[]) => Algorithm, current: string, ...secrets: string[]): Algorithms {
+		const [first, ...remainder] = secrets.map(part =>
+			Object.fromEntries(part.split(",").map(secret => secret.split(":", 2).map(item => item.trim())))
+		)
+		const result = Object.assign({}, ...(
+			Object.entries(first)
+				.map<[string, string[]]>(([name, secret]) => [name, [secret, ...remainder.map(part => part[name]).filter(part => part)]])
+				.map(([name, secrets]) => ({ get [name]() { return Object.assign(create(secrets), { name }) }}))
+		))
+		return { current: result[current], ...result }
+	}
+}
