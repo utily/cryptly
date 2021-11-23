16e01cd55f855551f7123c59f20e527c096e4eab
Simon Mika <simon@mika.se>
2021-11-23 12:14:23 +0100

    Fixed ESLint parsing issue.

--- Algorithms.ts
+++ Algorithms.ts
@@ -1,17 +1,26 @@
 import { Algorithm } from "./Algorithm"
+import { Tuple2 } from "./Tuple"
 
 export type Algorithms = Record<string, Algorithm> & { current: Algorithm }
 
-export namespace Algorithms {
+export namespace PrimarySecrets {
 	export function create(create: (keys: string[]) => Algorithm, current: string, ...secrets: string[]): Algorithms {
 		const [first, ...remainder] = secrets.map(part =>
 			Object.fromEntries(part.split(",").map(secret => secret.split(":", 2).map(item => item.trim())))
 		)
-		const result = Object.assign({}, ...(
-			Object.entries(first)
-				.map<[string, string[]]>(([name, secret]) => [name, [secret, ...remainder.map(part => part[name]).filter(part => part)]])
-				.map(([name, secrets]) => ({ get [name]() { return Object.assign(create(secrets), { name }) }}))
-		))
+		const result = Object.assign(
+			{},
+			...Object.entries(first)
+				.map<Tuple2<string, string[]>>(([name, secret]) => [
+					name,
+					[secret, ...remainder.map(part => part[name]).filter(part => part)],
+				])
+				.map(([name, secrets]) => ({
+					get [name]() {
+						return Object.assign(create(secrets), { name })
+					},
+				}))
+		)
 		return { current: result[current], ...result }
 	}
 }
