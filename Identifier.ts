bf4338377cd223ccde33821894a9fadfe05ae913
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2022-10-12 11:43:37 +0200

    Added min, max, next, and previous functions. (#41)
    Co-authored-by: Anton Kroner <anton.korner@paxport.net>

--- Identifier.ts
+++ Identifier.ts
@@ -51,6 +51,19 @@ export namespace Identifier {
 			result = result.slice(0, length)
 		return result.join("")
 	}
+	export function min(length: Length): Identifier {
+		return "".padStart(length, "A")
+	}
+	export function max(length: Length): Identifier {
+		return "".padStart(length, "_")
+	}
+	export function next(identifier: Identifier, increment = 1): Identifier {
+		const result = Base64.next(identifier, increment, "url")
+		return result.length == identifier.length ? result : result.substring(result.length - identifier.length)
+	}
+	export function previous(identifier: Identifier, decrement = 1): Identifier {
+		return next(identifier, -decrement)
+	}
 	export const length = [
 		4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112,
 		116, 120, 124, 128,
