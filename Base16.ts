fdf6a9636c2ef347f0eca7bac639f7b2b93174db
Björn Seifert <55486503+NordenBear@users.noreply.github.com>
2023-04-16 08:18:37 +0200

    Base64 operations (#46)
    * Added operations to Base64 and Base16
    
    * Removed global. no longer needed,giving problems

--- Base16.ts
+++ Base16.ts
@@ -1,7 +1,13 @@
+import * as ArrayBuffer from "./ArrayBuffer"
 import { TextEncoder } from "./TextEncoder"
 
-export function encode(value: Uint8Array | string, length?: number): string {
-	const data = typeof value == "string" ? new TextEncoder().encode(value) : value
+export function encode(value: ArrayBuffer | Uint8Array | string, length?: number): string {
+	const data =
+		typeof value == "string"
+			? new TextEncoder().encode(value)
+			: value instanceof Uint8Array
+			? value
+			: new Uint8Array(value)
 	let result: string[] = []
 	for (const d of data)
 		result.push(Math.floor(d / 16).toString(16), (d % 16).toString(16))
@@ -16,4 +22,16 @@ export function decode(value: string): Uint8Array {
 	for (let index = 0; index < result.length; index++)
 		result[index] = Number.parseInt(value[index * 2], 16) * 16 + Number.parseInt(value[index * 2 + 1], 16)
 	return result
+}
+export function xor(data: string[]): string {
+	return encode(ArrayBuffer.xor(...data.map(decode)))
+}
+export function bytewiseAdd(data: string[]): string {
+	return encode(ArrayBuffer.bytewiseAdd(...data.map(decode)))
+}
+export function add(data: string[]): string {
+	return encode(ArrayBuffer.add(...data.map(decode)))
+}
+export function combine(data: string[]): string {
+	return encode(ArrayBuffer.combine(...data.map(decode)))
 }
--- index.ts
+++ index.ts
@@ -1,13 +1,55 @@
-export { Algorithm } from "./Algorithm"
-export { Algorithms } from "./Algorithms"
-export * as Base16 from "./Base16"
-export * as Base64 from "./Base64"
-export { Digest } from "./Digest"
-export { Encrypted } from "./Encrypted"
-export { Identifier } from "./Identifier"
-export { Password } from "./Password"
-export { RandomValue } from "./RandomValue"
-export { Signer } from "./Signer"
-export { TextDecoder } from "./TextDecoder"
-export { TextEncoder } from "./TextEncoder"
 export * as cryptly from "./cryptly"
+
+// Deprecated exports
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { Algorithm } from "./Algorithm"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { Algorithms } from "./Algorithms"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export * as Base16 from "./Base16"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export * as Base64 from "./Base64"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { Digest } from "./Digest"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { Encrypted } from "./Encrypted"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { Identifier } from "./Identifier"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { Password } from "./Password"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { RandomValue } from "./RandomValue"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { Signer } from "./Signer"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { TextDecoder } from "./TextDecoder"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export { TextEncoder } from "./TextEncoder"
+/**
+ * @deprecated Use `import { cryptly } from "cryptly"`.
+ */
+export * as ArrayBuffer from "./ArrayBuffer"
