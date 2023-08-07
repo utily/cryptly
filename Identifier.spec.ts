787117979ceecbadc3e06d23aeaecef6750fc6b2
Elias Eriksson <eliaseriksson95@gmail.com>
2023-08-07 16:39:38 +0200

    pattern test

--- Identifier.spec.ts
+++ Identifier.spec.ts
@@ -92,7 +92,14 @@ describe("Identifier", () => {
 	it.each([4, 8, 12, 16, 64] as const)(`ordered id is`, (length: cryptly.Identifier.Length) => {
 		expect(cryptly.Identifier.is(cryptly.Identifier.generate(length, "ordered", length), length)).toBeTruthy()
 	})
-	const now = new Date().getTime()
+	const now = 1691418818480
+	it("pattern", () => {
+		expect(cryptly.Identifier.generate(4, "ordered", now)).toMatch(/^gESe$/)
+		expect(cryptly.Identifier.generate(8, "ordered", now)).toMatch(/^gESeo7Z0$/)
+		expect(cryptly.Identifier.generate(12, "ordered", now)).toMatch(/^gESeo7Z0---[\w\d-_]{1}$/)
+		expect(cryptly.Identifier.generate(16, "ordered", now)).toMatch(/^gESeo7Z0---[\w\d-_]{5}$/)
+		expect(cryptly.Identifier.generate(64, "ordered", now)).toMatch(/^gESeo7Z0---[\w\d-_]{53}$/)
+	})
 	it.each([4, 8, 12, 16, 64] as const)(`ordered id is ordered`, (length: cryptly.Identifier.Length) => {
 		expect(
 			cryptly.Identifier.generate(length, "ordered", now + 1) > cryptly.Identifier.generate(length, "ordered", now)
