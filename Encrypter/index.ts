cf72a710324c76a4c8c056b4af7b5f058f38f7a1
ChriJ <christofferj93@gmail.com>
2024-01-09 17:28:21 +0100

    remove comments

--- Encrypter/index.ts
+++ Encrypter/index.ts
@@ -1,11 +1,8 @@
 import { Aes as EncrypterAes } from "./Aes"
-// import { Rsa as EncrypterRsa } from "./Rsa"
 
-export type Encrypter = Encrypter.Aes // | Encrypter.Rsa
+export type Encrypter = Encrypter.Aes
 
 export namespace Encrypter {
 	export type Aes = EncrypterAes
 	export const Aes = EncrypterAes
-	// export type Rsa = EncrypterRsa
-	// export const Rsa = EncrypterRsa
 }
